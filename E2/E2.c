6e01f30afe6d8416911d6935e38f9cc84fb20a4f
Diego <Dih.cvel@gmail.com>
2021-07-08 22:47:33 -0300

    entradade dados

---
+++ E2/E2.c
@@ -0,0 +1,26 @@
+/*
+ * UTFPR - UNIVERSIDADE TECNOLÓGICA FEDERAL DO PARANÁ - CAMPUS TOLEDO
+ *
+ * Alunos: Diego da Silva, Kassiano Shiguematsu
+ * Professor: LUIS CARLOS MATHIAS
+ * 
+ * 
+ * E2 - Testar o E1 em alguma linguagem de baixo nível como o C
+ */ 
+#include <stdio.h>
+
+int main()
+{
+    // A    -> complemento do dividendo 
+    // d    -> divisor
+    // N    -> dividendo
+    // cont -> numero de bits do divisor 
+    long A = 0, d, N, cont;
+
+    printf("Denominador\n>>");
+    scanf("%ld", &N);
+    printf("Divisor\n>>");
+    scanf("%ld", &d);
+
+    return 0;
+}
